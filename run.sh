32b92bc941909bb92e2f92300c485388e3b1b666
Maximiliano Osorio <maxiosorio@gmail.com>
2024-05-27 10:56:47 -0400

    Release v0.0.2 (#2)

--- run.sh
+++ run.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-# $1: input file
-# $2: output file
+set -xe
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: run.sh <input_file> <output_file>"
-    exit 1
-fi
-
-# Run the main.py file with the input and output file
-python main.py ${1} ${2}
+cd ${_tapisExecSystemInputDir}
+python /code/main.py billing.csv ${_tapisExecSystemOutputDir}/output.txt
