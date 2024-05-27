113ad2c8f84d16b3badf136b95ede86c4ba8f709
Maximiliano Osorio <maxiosorio@gmail.com>
2024-05-27 09:58:52 -0400

    fix: remove appArgs

--- run.sh
+++ run.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-# $1: input file
-
-if [ "$#" -ne 1 ]; then
-    echo "Usage: run.sh <input_file>"
-    exit 1
-fi
-
-# Run the main.py file with the input and output file
-python main.py ${1} ${WORK}/output.txt
+python main.py fileToModify=$_tapisExecSystemInputDir/billing.csv ${WORK}/output.txt
